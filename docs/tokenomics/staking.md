5df33b18e2e1fd676eae0ad9ef4c8cff76e8fb79
Solana Plutus <143905248+SolanaPlutus@users.noreply.github.com>
2023-09-07 15:51:55 +0200

    New pages

---
+++ docs/tokenomics/staking.md
@@ -0,0 +1,8 @@
+---
+sidebar_position: 10
+title: Staking
+sidebar_label: Staking
+---
+
+# Staking
+ 
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/overview">
+            to="/docs/overview/litepaper">
             📜 LitePaper 📜 
           </Link>
         </div>
